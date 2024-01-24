16355adfd29b637ff5b75f5227153fd0012cbca0
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2024-01-24 19:37:47 +0300

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,31 @@
+import path from "path";
+import { open, close, writeFile } from "node:fs";
+
+const content = "I am fresh and young";
+const FOLDER_NAME = "files";
+const FILE_NAME = "fresh.txt";
+const ERROR_MESSAGE = "FS operation failed";
+const pathToFile = path.join(import.meta.dirname, FOLDER_NAME, FILE_NAME);
+
 const create = async () => {
-    // Write your code here 
+    open(pathToFile, "wx", (err, fd) => {
+      if (err) {
+        if (err.code === "EEXIST") {
+          throw new Error(ERROR_MESSAGE);
+        }
+        throw err;
+      }
+
+      try {
+        writeFile(pathToFile, content, "utf8", (err) => {
+          if (err) throw err;
+        });
+      } finally {
+        close(fd, (err) => {
+          if (err) throw err;
+        });
+      }
+    });
 };
 
 await create();

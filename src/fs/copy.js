5fc9a8bc4e304ad8912f7a117bf960a519bd7a27
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2024-01-25 01:08:12 +0300

    feat: implement copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,32 @@
+import { dirname, join } from "path";
+import { readdir, mkdir, copyFile } from "node:fs/promises";
+import { fileURLToPath } from "url";
+import { error } from "console";
+
+const sourceFolderName = "files";
+const destinationFolderName = "files_copy";
+const ERROR_MESSAGE = "FS operation failed";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const sourcePath = join(__dirname, sourceFolderName);
+const destinationPath = join(__dirname, destinationFolderName);
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    const files = await readdir(sourcePath);
+    await mkdir(destinationPath);
+    await Promise.all(
+      files.map(async (file) => {
+        const src = join(sourcePath, file);
+        const dest = join(destinationPath, file);
+        await copyFile(src, dest).catch(error);
+      })
+    );
+  } catch (error) {
+    if (error.code === "ENOENT" || error.code === "EEXIST") {
+      throw new Error(ERROR_MESSAGE);
+    }
+  }
 };
 
 await copy();

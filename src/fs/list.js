16bd65cc1c1943b4f7955909b16bde1b08c15421
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2024-01-25 13:39:27 +0300

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { readdir } from "node:fs/promises";
+import { error } from "console";
+
+const ERROR_MESSAGE = "FS operation failed";
+const sourceFolderName = "files";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const sourcePath = join(__dirname, sourceFolderName);
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const files = await readdir(sourcePath);
+    files.forEach((file) => console.log(file));
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      throw new Error(ERROR_MESSAGE);
+    }
+  }
 };
 
-await list();
+await list().catch(error);

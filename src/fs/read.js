9eb4be66f35ad38fab5086c0d6e7fcd9cb0f1b11
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2024-01-25 14:43:09 +0300

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { readFile } from "node:fs/promises";
+import { error, log } from "console";
+
+const ERROR_MESSAGE = "FS operation failed";
+const sourceFolderName = "files";
+const fileToRead = "fileToRead.txt";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const fileToReadPath = join(__dirname, sourceFolderName, fileToRead);
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const content = await readFile(fileToReadPath, { encoding: "utf-8" });
+    log(content);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      throw new Error(ERROR_MESSAGE);
+    }
+  }
 };
 
-await read();
+await read().catch(error);

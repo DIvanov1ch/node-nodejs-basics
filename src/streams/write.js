eb4a9caaa7a2468e8c4545ea6b20923f8b14dcfc
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2024-01-28 13:33:33 +0300

    feat: implement write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { createWriteStream } from "fs";
+import { stdin } from "process";
+
+const FOLDER_NAME = "files";
+const FILE_NAME = "fileToWrite.txt";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filepath = join(__dirname, FOLDER_NAME, FILE_NAME);
+
 const write = async () => {
-    // Write your code here 
+  stdin.pipe(createWriteStream(filepath));
 };
 
 await write();

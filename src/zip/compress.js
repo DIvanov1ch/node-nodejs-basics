b659a71746ddabfbbb7d53da22681af96206f463
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2024-01-28 14:53:05 +0300

    feat: implement compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { createGzip } from "zlib";
+import { pipeline } from "stream/promises";
+import { createReadStream, createWriteStream } from "fs";
+
+const FOLDER_NAME = "files";
+const fileToCompress = "fileToCompress.txt";
+const fileNameAfterCompress = "archive.gz";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const source = join(__dirname, FOLDER_NAME, fileToCompress);
+const destination = join(__dirname, FOLDER_NAME, fileNameAfterCompress);
+
 const compress = async () => {
-    // Write your code here 
+  pipeline(
+    createReadStream(source),
+    createGzip(),
+    createWriteStream(destination)
+  );
 };
 
 await compress();

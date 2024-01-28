505f4b15db761dcc406ba55104ad9bb05f821607
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2024-01-28 16:16:03 +0300

    feat: add catch error to pipeline in compress file

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -3,21 +3,22 @@ import { fileURLToPath } from "url";
 import { createGzip } from "zlib";
 import { pipeline } from "stream/promises";
 import { createReadStream, createWriteStream } from "fs";
+import { error } from "console";
 
 const FOLDER_NAME = "files";
 const fileToCompress = "fileToCompress.txt";
-const fileNameAfterCompress = "archive.gz";
+const compressedFile = "archive.gz";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const source = join(__dirname, FOLDER_NAME, fileToCompress);
-const destination = join(__dirname, FOLDER_NAME, fileNameAfterCompress);
+const destination = join(__dirname, FOLDER_NAME, compressedFile);
 
 const compress = async () => {
   pipeline(
     createReadStream(source),
     createGzip(),
     createWriteStream(destination)
-  );
+  ).catch(error);
 };
 
 await compress();

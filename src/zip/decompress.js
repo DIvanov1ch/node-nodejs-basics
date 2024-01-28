003a002e970266c879c10341f8a5a7ae72533ba1
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2024-01-28 16:14:58 +0300

    feat: implement decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,27 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { createGunzip } from "zlib";
+import { pipeline } from "stream/promises";
+import { createReadStream, createWriteStream } from "fs";
+
+const FOLDER_NAME = "files";
+const fileToDecompress = "archive.gz";
+const decompressedFile = "fileToCompress.txt";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const source = join(__dirname, FOLDER_NAME, fileToDecompress);
+const destination = join(__dirname, FOLDER_NAME, decompressedFile);
+
 const decompress = async () => {
-    // Write your code here 
+  pipeline(
+    createReadStream(source),
+    createGunzip(),
+    createWriteStream(destination)
+  ).catch((err) => {
+    if (err.code === "ENOENT") {
+      console.log(`No such file or directory ${fileToDecompress}`);
+    }
+  });
 };
 
 await decompress();

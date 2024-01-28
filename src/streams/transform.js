31da928cdc43ef0a13b4e001c569c4da6289513d
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2024-01-28 14:30:06 +0300

    feat: implement transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import { createReadStream } from "fs";
+import { stdin, stdout } from "process";
+import { Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  const reverse = new Transform({
+    transform(data, _encoding, callback) {
+      const reversedData = data
+        .toString()
+        .split("")
+        .reverse()
+        .join("")
+        .concat("\n");
+      callback(null, reversedData);
+    },
+  });
+  stdin.pipe(reverse).pipe(stdout);
 };
 
 await transform();

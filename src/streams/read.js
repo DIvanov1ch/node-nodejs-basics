de9f45c301b9dafed27a189713006cb04835d577
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2024-01-28 13:33:07 +0300

    feat: implement read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { createReadStream } from "fs";
+import { stdout } from "process";
+
+const FOLDER_NAME = "files";
+const FILE_NAME = "fileToRead.txt";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filepath = join(__dirname, FOLDER_NAME, FILE_NAME);
+
 const read = async () => {
-    // Write your code here 
+  createReadStream(filepath).pipe(stdout);
 };
 
 await read();

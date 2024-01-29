cb88d0c78d22c193f500bac4c0d847ceffe72a5a
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2024-01-29 19:08:55 +0300

    feat: make copy() recursive

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,7 +1,7 @@
-import { dirname, join } from "path";
+import { dirname, join } from "node:path";
 import { readdir, mkdir, copyFile } from "node:fs/promises";
-import { fileURLToPath } from "url";
-import { error } from "console";
+import { fileURLToPath } from "node:url";
+import { error } from "node:console";
 
 const sourceFolderName = "files";
 const destinationFolderName = "files_copy";
@@ -12,21 +12,31 @@ const sourcePath = join(__dirname, sourceFolderName);
 const destinationPath = join(__dirname, destinationFolderName);
 
 const copy = async () => {
-  try {
-    const files = await readdir(sourcePath);
-    await mkdir(destinationPath);
-    await Promise.all(
-      files.map(async (file) => {
-        const src = join(sourcePath, file);
-        const dest = join(destinationPath, file);
-        await copyFile(src, dest).catch(error);
-      })
-    );
-  } catch (error) {
-    if (error.code === "ENOENT" || error.code === "EEXIST") {
-      throw new Error(ERROR_MESSAGE);
+  const copyDir = async (src, dest) => {
+    try {
+      const files = await readdir(src, {
+        withFileTypes: true,
+      });
+      await mkdir(dest);
+      await Promise.all(
+        files.map(async (file) => {
+          const source = join(src, file.name);
+          const destination = join(dest, file.name);
+          if (file.isFile()) {
+            await copyFile(source, destination).catch(error);
+          }
+          if (file.isDirectory()) {
+            await copyDir(source, destination);
+          }
+        })
+      );
+    } catch (error) {
+      if (error.code === "ENOENT" || error.code === "EEXIST") {
+        throw new Error(ERROR_MESSAGE);
+      }
     }
-  }
+  };
+  await copyDir(sourcePath, destinationPath);
 };
 
 await copy().catch(error);

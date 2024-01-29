42c78bcb7b19cd8594cd9ef70a296f3516ca1491
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2024-01-29 11:37:54 +0300

    feat: implement spawnChildProcess function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,19 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { fork } from "node:child_process";
+import { stdout, stdin } from "process";
+
+const FOLDER_NAME = "files";
+const FILE_NAME = "script.js";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filepath = join(__dirname, FOLDER_NAME, FILE_NAME);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = fork(filepath, args, { silent: true });
+  child.stdout.pipe(stdout);
+  stdin.pipe(child.stdin);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["someArgument1", "someArgument2", "child"]);
